8a8e6b2dac5bb798e9658fba4cc5a6f2defd584f
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2023-08-18 10:38:55 -0300

    projeto atualizado, model GUEST criado

---
+++ app/src/main/java/com/example/guestsapp/GuestFormViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.guestsapp
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/guestsapp/Guests/Guest.kt
@@ -0,0 +1,4 @@
+package com.example.guestsapp.Guests
+
+class Guest(val id:Int,val name:String,val presence:Boolean) {
+}
